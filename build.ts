2bae6800946fc091a51db7e4958b335f22fe7aae
sevenc-nanashi <sevenc7c@sevenc7c.com>
2024-06-14 21:32:42 +0900

    Add: homepageを追加

--- build.ts
+++ build.ts
@@ -5,6 +5,7 @@ const bannerMap: Record<string, string> = {
   name: "コネコネ（仮）",
   description: "ニコニコ（Re:仮）の補助ツール。",
   version: `v${packageJson.version}`,
+  homepage: packageJson.homepage,
   match: "https://www.nicovideo.jp/*",
   updateURL: "https://sevenc7c.com/conecone-kakkokari/index.user.js",
   downloadURL: "https://sevenc7c.com/conecone-kakkokari/index.user.js",
