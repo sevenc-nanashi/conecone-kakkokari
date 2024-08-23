47cda54ba7727b4297cfe3dac204f990175a9ba6
sevenc-nanashi <sevenc7c@sevenc7c.com>
2024-08-23 12:37:21 +0900

    Delete: 爆破

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # コネコネ（仮）/ ニコニコ（Re:仮）補助ツール
 
+このツールはニコニコの復旧に伴い、爆破されました。
+いままでのご利用ありがとうございました。
+
+[メッセージ](./MESSAGE.md)
+
+---
+
 コネコネ（仮）は、ニコニコ（Re:仮）の補助ツールです。
 
 ## 機能
@@ -15,7 +22,7 @@
 
    - 動作確認済：
      - [Tampermonkey](https://www.tampermonkey.net/)（Windows 10、Firefox / Chrome）
-     - [Userscrips](https://apps.apple.com/jp/app/userscripts/id1463298887)（iOS 17、Safari）
+     - [Userscripts](https://apps.apple.com/jp/app/userscripts/id1463298887)（iOS 17、Safari）
 
 2. このURLを読み込みます：[`https://raw.githubusercontent.com/sevenc-nanashi/conecone-kakkokari/built/index.user.js`](https://raw.githubusercontent.com/sevenc-nanashi/conecone-kakkokari/built/index.user.js)
 
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,57 @@
-import packageJson from "../package.json";
-import * as ngList from "./ngList.ts";
-import * as fullScreen from "./fullScreen.ts";
-import * as styles from "./styles.ts";
-import * as doubleTap from "./doubleTap.ts";
+import van from "vanjs-core";
 
-console.log(
-  "" +
-    "%c== コネコネ（仮） ========================================\n" +
-    `  %cニコニコ（Re:仮）の補助ツール。\n` +
-    `  %cVersion: %cv${packageJson.version}\n` +
-    `  Developed by %cNanashi.\n` +
-    `  %c${packageJson.homepage}\n` +
-    "----------------------------------------------------------\n",
-  "color: #18b4e6",
-  "color: auto",
-  "color: #18b4e6",
-  "color: auto",
-  "color: #48b0d5",
-  "color: #18b4e6",
-);
+const { a, div, path, span } = van.tags;
+const { svg } = van.tags("http://www.w3.org/2000/svg");
+const modifier = () => {
+  if (localStorage.getItem("conecone-rikari-read")) {
+    return;
+  }
+  const target = document.querySelector<HTMLDivElement>(
+    ".nico-CommonHeaderRoot",
+  );
+  if (!target) {
+    setTimeout(modifier, 100);
+    return;
+  }
+  const svgElem = svg({
+    viewBox: "0 0 24 24",
+    fill: "none",
+    class: "common-header-3drezb",
+  });
+  svgElem.innerHTML = `
+  <path fill-rule="evenodd" clip-rule="evenodd"
+        d="M17.357 12a.498.498 0 01-.146.356l-8.29 8.29a.5.5 0 01-.708 0l-1.41-1.41a.5.5 0 010-.707L13.333 12l-6.53-6.53a.5.5 0
+        010-.707l1.41-1.41a.5.5 0 01.707 0l8.29 8.29a.499.499 0 01.147.357Z"
+        style="fill: rgb(24, 180, 230) !important;"></path>`.replace(
+    /\s+(\s)/g,
+    "$1",
+  );
+  target.insertBefore(
+    div(
+      { class: "common-header-fqlnpx" },
+      div(
+        { class: "common-header-1fxu773", style: "background-color: #18b4e6;" },
+        a(
+          {
+            target: "_blank",
+            href: "https://github.com/sevenc-nanashi/conecone-kakkokari/blob/main/MESSAGE.md",
+            class: "common-header-1524g3q",
+            onclick: () => localStorage.setItem("conecone-rikari-read", "true"),
+          },
+          div(
+            { class: "common-header-11rms0d" },
+            div(
+              { class: "common-header-1gedanw" },
+              div({ class: "common-header-1udne4d" }, "コネコネ（仮）について"),
+              span({ class: "common-header-sgyyjv" }, svgElem),
+            ),
+          ),
+        ),
+      ),
+    ),
 
-styles.main();
+    target.firstChild,
+  );
+};
 
-for (const script of [ngList.inject]) {
-  const scriptElement = document.createElement("script");
-  scriptElement.textContent = `(${script.toString()})()`;
-  document.body.appendChild(scriptElement);
-}
-
-ngList.main();
-fullScreen.main();
-doubleTap.main();
+modifier();
